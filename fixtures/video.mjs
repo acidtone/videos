47e790c8c3bbd17f2d49893999082e4be4278310
Tony Grimes <acidtone@tonygrimes.com>
2020-03-22 11:01:15 -0600

    Added basic button events for YT timestamps

--- fixtures/video.mjs
+++ fixtures/video.mjs
@@ -1,13 +1,11 @@
-export const videos = [
-  {
-    id: 'panKf9hzUfQ',
-    timestamps: [
-      41: 'Is CSS a programming language?',
-      168: 'What is a programming language?',
-      342: 'Turd Driven Development',
-      472: 'What is programming?',
-      512: 'Algorithms 101',
-      725: 'CSS Algorithms 101'
-    ]
-  }
-];
+export const video = {
+  id: 'panKf9hzUfQ',
+  timestamps: [
+    41: 'Is CSS a programming language?',
+    168: 'What is a programming language?',
+    342: 'Turd Driven Development',
+    472: 'What is programming?',
+    512: 'Algorithms 101',
+    725: 'CSS Algorithms 101'
+  ]
+};
--- js/app.mjs
+++ js/app.mjs
@@ -2,37 +2,45 @@
 // import { videos } from '../fixtures/videos.mjs';
 
 function init() {
-  const videos = [
-    {
-      id: 'panKf9hzUfQ',
-      title: 'CSS Algorithms',
-      attribution: 'Lara Schenck',
-      timestamps: {
-        41: 'Is CSS a programming language?',
-        168: 'What is a programming language?',
-        342: 'Turd Driven Development',
-        472: 'What is programming?',
-        512: 'Algorithms 101',
-        725: 'CSS Algorithms 101'
-      }
+  const video = {
+    id: 'panKf9hzUfQ',
+    timestamps: {
+      41: 'Is CSS a programming language?',
+      168: 'What is a programming language?',
+      342: 'Turd Driven Development',
+      472: 'What is programming?',
+      512: 'Algorithms 101',
+      725: 'CSS Algorithms 101'
     }
-  ];
+  };
   
   const vidTemplate = document.querySelector("#video");
   const tsTemplate = document.querySelector("#timestamp");
-  videos.forEach(function(video){
-    const vidClone = vidTemplate.content.cloneNode(true);
-    
-    vidClone.querySelector('h2').innerText = video.title;
-    vidClone.querySelector('address').innerText = video.attribution;
-    const tsList = vidClone.querySelector('ul');
-    console.log(tsList);
-    for(const timestamp in video.timestamps) {
-      // TODO: preventDefault on a and use YT iFrame API instead
-      tsList.innerHTML += `<li><a href="https://www.youtube.com/watch?v=${video.id}&t=${timestamp}">${video.timestamps[timestamp]}</a></li>`;
-    }
-    document.querySelector('section').appendChild(vidClone);
+  const vidClone = vidTemplate.content.cloneNode(true);
+  
+  vidClone.querySelector('h2').innerText = video.title;
+  vidClone.querySelector('address').innerText = video.attribution;
+  const tsList = vidClone.querySelector('ul');
+
+  Object.entries(video.timestamps).forEach(function(title){
+    console.log(title[0]);
+    tsList.innerHTML += `<li><button value="${title[0]}">${title[1]}</button></li>`;
   })
+
+  // Add click listeners to each button; clunky but it works for now
+  // Using for...of and NOT for...in as per this SO:
+  // https://stackoverflow.com/questions/22754315/for-loop-for-htmlcollection-elements
+
+  const buttons = tsList.querySelectorAll('button');
+  for (let button of buttons) {
+    button.addEventListener('click',function(evt){
+      console.log(this.value); 
+      player.seekTo(this.value);
+    })
+  }
+
+  document.querySelector('section').appendChild(vidClone);
+  
 }
 
 document.addEventListener('load',init());
