887d48a0148d497a7c4ba1a2fc6e09a42d7e061e
Tony Grimes <acidtone@tonygrimes.com>
2020-04-04 14:32:26 -0600

    added support for query parameters

--- js/app.mjs
+++ js/app.mjs
@@ -1,14 +1,11 @@
-// Import fixture. POC uses only one video for testing
-// TODO: Add support for multiple videos by adding url parameters for vid id and timestamps
-// TODO: migrate fixture data to MongoDB.
+// TODO: migrate fixture data to MongoDB OR build MongoDB into a static website generator.
 import { videos } from '../fixtures/videos.mjs';
 const video = videos.filter(function(item){
   return item.id === videoId;
 })[0] || {};
 
 function init() {
-  console.log(video.timestamps);
-  if (typeof video.timestamps === "object") {
+    if (typeof video === "object" && typeof video.timestamps === "object") {
     const tsList = document.querySelector('template#timestamps').content.cloneNode(true);
     const tsItemTemplate = tsList.querySelector('li').cloneNode(true);
     const tsParent = tsList.parentNode;
@@ -18,12 +15,17 @@ function init() {
 
     tsList.textContent = '';
 
-    Object.entries(video.timestamps).forEach(function(ts){
-      let listItem = tsItemTemplate.cloneNode(true);
-      listItem.querySelector('.ts-click').setAttribute('value',ts[0]);
-      listItem.querySelector('.ts-click').innerText = ts[1];
-      tsList.appendChild(listItem);
-    })
+    if (Object.keys(video.timestamps).length > 0) {
+      Object.entries(video.timestamps).forEach(function(ts){
+        let listItem = tsItemTemplate.cloneNode(true);
+
+        listItem.querySelector('.ts-click').setAttribute('value',ts[0]);
+        listItem.querySelector('.ts-click').innerText = ts[1];
+        tsList.appendChild(listItem);
+      })
+    } else {
+      document.querySelector('template#timestamps').parentNode.innerText = 'No bookmarks found for this video.';
+    }
 
     // Add click listeners to each button; clunky but it works for now
     // Using for...of and NOT for...in as per this SO:
@@ -34,12 +36,16 @@ function init() {
       button.addEventListener('click',function(evt){
         // TODO: add 't' search param to the address bar
         player.seekTo(this.value);
+        url.searchParams.set("t",this.value);
+        console.log(url.searchParams.get("t"))
+        history.replaceState({}, "", url.search);
       })
     }
 
     document.querySelector('template#timestamps').parentNode.appendChild(tsList);
   } else {
-    document.querySelector('template#timestamps').parentNode.innerText = 'No bookmarks found for this video.';
+    // TODO: create list of videos
+    console.log('nope.')
   }
 }
 
